94fccf35816291f9f2e91907a4ea0dbddda3aa28
Pedro Augusto <pedroaugusto@Pedros-MacBook-Pro.local>
2024-07-15 22:44:43 -0300

    comentando leetcode778

--- leetcode_778/swim_in_rising_water.cpp
+++ leetcode_778/swim_in_rising_water.cpp
@@ -6,44 +6,48 @@ using namespace std;
 
 class Solution {
     
+    // Definição da estrutura Tupla para representar cada elemento na fila de prioridade
     struct Tupla {
         int pesoMaximo;
         int x;
         int y;
         
+        // Construtor da Tupla para inicializar seus membros
         Tupla(int pesoMaximo, int x, int y) : pesoMaximo(pesoMaximo), x(x), y(y) {}
         
+        // Operador '>' sobrecarregado para a fila de prioridade, usando pesoMaximo como critério de ordenação
         bool operator>(const Tupla& outro) const {
             return pesoMaximo > outro.pesoMaximo;
         }
     };
     
 public:
+    // Função principal que calcula o caminho mínimo para nadar na água representada pela grade
     int swimInWater(vector<vector<int>>& grade) {
-        int n = grade.size();
+        int n = grade.size(); // Tamanho da grade (n x n)
         
-        priority_queue<Tupla, vector<Tupla>, greater<Tupla>> pq;
-        vector<vector<int>> visitado(n, vector<int>(n, 0));
+        priority_queue<Tupla, vector<Tupla>, greater<Tupla>> pq; // Fila de prioridade mínima para buscar o menor peso
+        vector<vector<int>> visitado(n, vector<int>(n, 0)); // Matriz para controlar os nós visitados
         
-        pq.push(Tupla(grade[0][0], 0, 0));
+        pq.push(Tupla(grade[0][0], 0, 0)); // Inicializa a fila de prioridade com o peso da posição inicial (0, 0)
         
         while (!pq.empty()) {
-            Tupla t = pq.top();
-            pq.pop();
-            int x = t.x;
+            Tupla t = pq.top(); // Obtém o topo da fila de prioridade (menor peso atual)
+            pq.pop(); 
+            int x = t.x; 
             int y = t.y;
-            int peso = t.pesoMaximo;
-            
+            int peso = t.pesoMaximo; 
             if (visitado[x][y] == 1) {
-                continue;
+                continue; // Se o nó já foi visitado, continua para o próximo
             }
             
-            visitado[x][y] = 1;
+            visitado[x][y] = 1; // Marca o nó como visitado
             
             if (x == n - 1 && y == n - 1) {
-                return peso;
+                return peso; // Se chegou ao destino (última posição), retorna o peso máximo até então
             }
             
+            // Verifica os nós adjacentes e os adiciona à fila de prioridade se ainda não foram visitados
             if (x > 0 && visitado[x - 1][y] == 0) {
                 int pesoAtual = max(peso, grade[x - 1][y]);
                 pq.push(Tupla(pesoAtual, x - 1, y));
@@ -62,6 +66,6 @@ public:
             }
         }
         
-        return 0;
+        return 0; // Retorna 0 se não houver caminho válido encontrado
     }
 };
